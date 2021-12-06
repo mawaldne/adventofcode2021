5481e2183d4d7fd68f49b5f51ca2de72eeefa04f
Mike Waldner <mike.waldner@gmail.com>
2021-12-06 12:34:02 -0700

    Day 4 part b

--- day4.py
+++ day4.py
@@ -1,7 +1,4 @@
 import fileinput
-import pprint
-import numpy as np
-pp = pprint.PrettyPrinter(indent=4)
 
 boards = []
 board = []
@@ -29,18 +26,29 @@ def transpose(board):
     return boardT
 
 
-def bingo(sequence, boards):
+def bingo(sequence, boards, last):
+    wins = set()
     drawn = set()
 
     for last_picked in sequence:
         drawn.add(last_picked)
 
-        for board in boards:
+        for i, board in enumerate(boards):
             for row in (board + transpose(board)):
-                if len(set(drawn) & set(row)) == 5:
-                    return (last_picked, drawn, board)
+                if len(set(drawn) & set(row)) == len(row):
+                    wins.add(i)
+                    if not last or len(wins) == len(boards):
+                        return (last_picked, drawn, board)
 
-last_picked, drawn, board = bingo(draws, boards)
+last_picked, drawn, board = bingo(draws, boards, False)
 total_unpicked_numbers = sum([sum(set(row) - drawn) for row in board])
 
 print(last_picked * total_unpicked_numbers)
+
+last_picked, drawn, last_board = bingo(draws, boards, True)
+total_unpicked_numbers = sum([sum(set(row) - drawn) for row in last_board])
+
+print(last_picked * total_unpicked_numbers)
+
+
+
